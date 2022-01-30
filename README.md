a5620dcce0fc127d26b29d676d35b29d74f09def
EirEoin <eoinc2010@live.ie>
2022-01-30 11:48:19 +0000

    Added computer board for the computer to guess against

--- README.md
+++ README.md
@@ -1,49 +1,49 @@
-#Board Battleship
+# Board Battleship
 
 Board Battleship is a python terminal game, which runs the Code Institute mock terminal on Heroku.
 
 The User can try beat the Computer by finding all of the battleships co-ordinates in a limited amount of guesses! One battleship occupies one square on the board.
 
 
-###How to play
+## How to play
 
 Board Battleships is based on the classic Battleships game . In this game the player starts the game and the computers battleship co-ordinates are randomly generated. '-' marks guessed and missed co-ordinates and 'X' marks hit battleships.
 The winner is either The Player after guessing all of the battleship co-ordinates, or the computer when the `player fails to do so.
 
-###Existing Features 
+## Existing Features 
 
--Random board is generated and the player cannot see where the computer's ships are.
+- Random board is generated and the player cannot see where the computer's ships are.
 
--Play against the computer.
+- Play against the computer.
 
--Accepts user input and rejects/reacts to invalid input.
+- Accepts user input and rejects/reacts to invalid input.
 
--Maintains score
+- Maintains score
 
--Input validation: 
+- Input validation: 
 You cannot enter coordinates outside of the grid.
 You cannot enter the same guess twice.
 You must enters letters in the row and numbers un the column.
 
-###Future Features
+## Future Features
 
 - Allow player to select the board size and number of ships
 - Create a user game board for the computer to guess against
 - Allow player to place their own ships on the Battlefield.
 -Have ships larger than 1x1
 
-##Data Model
+## Data Model
 I decided to use a Board class as my model. The game creates an instance for the computers board.
 
-##Testing
+## Testing
 
 I have manually tested the project by doing the following:
 
 - Passed the code through a PEP8 linter and confirmed there are no problems.
--Given invalid inputs: strings when numbers are expected and numbers when strings are expected.
--Tested by local terminal and Code Institute Heroku terminal.
+- Given invalid inputs: strings when numbers are expected and numbers when strings are expected.
+-T ested by local terminal and Code Institute Heroku terminal.
 
-##Bugs 
+## Bugs 
 Solved Bugs 
 - When I wrote the project I had syntax errors until I fixed them by revising past challenges.
 
@@ -54,10 +54,10 @@ Validator Testing
 - PEP8
 No errors were returned from PEP8online.com
 
-###Deployment 
+## Deployment 
 This project was deployed using Code Institute's ock terminal for Heroku. 
 
--Steps for deployment:
+- Steps for deployment:
 Fork or clone this respository
 
 Creates a new Heroku app
@@ -69,10 +69,10 @@ Link the Heroku app to the respository
 Click on Deploy
 
 
-##Credits 
+## Credits 
 
--Code Institute for the deployment terminal and Python lessons.
+- Code Institute for the deployment terminal and Python lessons.
 
--Codecademy Python2 free lessons.
+- Codecademy Python2 free lessons.
 
 - Wikipedia for the details of the Battleship game
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-## X for placing battleship and hit battleship
+# X for placing battleship and hit battleship
 """
 Symbols used in the Battleships game
 """
@@ -9,13 +9,14 @@ from random import randint
 
 player_board = [[' '] * 5 for x in range(5)]
 computer_board = [[' '] * 5 for x in range(5)]
+computer_hidden_board = [[' '] * 5 for x in range(5)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5}
 
 
 def print_board(board):
     """
-    Prints the Battleships to the playing field
+    Prints the Battleships playing field
     """
     print('   A B C D E')
     print('   ----------')
@@ -31,9 +32,19 @@ def populate_board(board):
     """
     for ship in range(5):
         ship_row, ship_column = randint(0, 4), randint(0, 4)
-        while board[ship_row][ship_column] == 'X':
+        while board[ship_row][ship_column] == '#':
             ship_row, ship_column = randint(0, 4), randint(0, 4)
-        board[ship_row][ship_column] = 'X'
+        board[ship_row][ship_column] = '#'
+
+
+def computer_guess():
+    """
+    Computer guess on battleship grid
+    """
+    ship_row, ship_column = randint(0, 4), randint(0, 4)
+    while player_board[ship_row][ship_column] == "X" or player_board[ship_row][ship_column] == "-":
+        ship_row, ship_column = randint(0, 4), randint(0, 4)
+    return ship_row, ship_column
 
 
 def get_ship_location():
@@ -41,11 +52,11 @@ def get_ship_location():
     Input for Rows and Columns along with some input validation
     """
     row = input(' Please enter a ship row 1 - 5')
-    while row not in '12345':
+    while row not in '12345' or len(row) != 1:
         print('Please enter a valid row')
         row = input(' Please enter a ship row 1 - 5')
     column = input('Please enter a ship column A - E').upper()
-    while column not in 'ABCDE':
+    while column not in 'ABCDE' or len(row) != 1:
         print('Please enter a valid column')
         column = input('Please enter a ship column A - E').upper()
     return int(row) - 1, letters_to_numbers[column]
@@ -63,26 +74,46 @@ def count_hits(board):
     return count
 
 
-populate_board(player_board)
-turns = 10
-while turns > 0:
-    print('WELCOME TO BATTLESHIP')
-    print_board(computer_board)
-    row, column = get_ship_location()
-    if computer_board[row][column] == '-':
-        print('You already guessed that')
-    elif computer_board[row][column] == 'X':
-        print(' Congratulations, you have hit the battleship!')
-        computer_board[row][column] = 'X'
-        turns -= 1
-    else:
-        print('Sorry you have missed')
-        computer_board[row][column] = '-'
-        turns -= 1
-    if count_hits(computer_board) == 5:
-        print('Congratulations, you have sunk all the battleships')
-        break
-    print('You have' + str(turns) + ' turns remaining')
-    if turns == 0:
-        print('Sorry, you ran out of turns the game is over')
-        break
+def main():
+    """
+    Runs the battleship game
+    """
+    populate_board(computer_hidden_board)
+    populate_board(player_board)
+    while count_hits(player_board) < 5 or count_hits(computer_board) < 5:
+        print('WELCOME TO BATTLESHIP')
+        print_board(computer_board)
+        print("COMPUTER BOARD")
+        print_board(player_board)
+        print("YOUR BOARD")
+
+        row, column = get_ship_location()
+        computer_row, computer_column = computer_guess()
+        if computer_hidden_board[row][column] == '-':
+            print('You already guessed that')
+        elif computer_hidden_board[row][column] == '#':
+            print(' Congratulations, you have hit the battleship!')
+            computer_board[row][column] = 'X'
+        else:
+            print('Sorry you have missed')
+            computer_board[row][column] = '-'
+        if count_hits(computer_hidden_board) == 5:
+            print('Congratulations, you have sunk all the battleships')
+            break
+
+        if player_board[computer_row][computer_column] == '#':
+            print('Oops, your battleship was hit!')
+            player_board[computer_row][computer_column] = 'X'
+        else:
+            print('That was a close one, computer missed')
+            player_board[computer_row][computer_column] = '-'
+        if count_hits(player_board) == 5:
+            print('Unlucky, all your battleships have been sunk')
+            break
+
+        if count_hits(computer_board) == 5:
+            print("Congratulations you won")
+
+
+if __name__ == "__main__":
+    main()
