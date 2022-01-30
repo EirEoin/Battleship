e2b7d8d501ee8a8bca32bf5a433a2c773a418599
EirEoin <eoinc2010@live.ie>
2022-01-30 00:13:34 +0000

    Fixed alot of my problems again after github reset my repository to where i was earlier today, deleting all of my new work

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-#X for placing battleship and hit battleship
+## X for placing battleship and hit battleship
+"""
+Symbols used in the Battleships game
+"""
 # '-' for available space
 # '-' for missed shot
 
@@ -7,24 +10,36 @@ from random import randint
 player_board = [[' '] * 5 for x in range(5)]
 computer_board = [[' '] * 5 for x in range(5)]
 
-letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5}
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5}
+
 
 def print_board(board):
+    """
+    Prints the Battleships to the playing field
+    """
     print('   A B C D E')
     print('   ----------')
-    row_number = 1 
+    row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 def populate_board(board):
+    """
+    Spawns ships on The Battlefield
+    """
     for ship in range(5):
-        ship_row, ship_column = randint(0,4), randint(0,4)
+        ship_row, ship_column = randint(0, 4), randint(0, 4)
         while board[ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0,4), randint(0,4)
+            ship_row, ship_column = randint(0, 4), randint(0, 4)
         board[ship_row][ship_column] = 'X'
 
+
 def get_ship_location():
+    """
+    Input for Rows and Columns along with some input validation
+    """
     row = input(' Please enter a ship row 1 - 5')
     while row not in '12345':
         print('Please enter a valid row')
@@ -35,13 +50,18 @@ def get_ship_location():
         column = input('Please enter a ship column A - E').upper()
     return int(row) - 1, letters_to_numbers[column]
 
+
 def count_hits(board):
-    count = 0 
+    """
+    Counts the number of Battleships hit
+    """
+    count = 0
     for row in board:
         for column in row:
             if column == 'X':
-                count =+ 1
+                count = + 1
     return count
+
 
 populate_board(player_board)
 turns = 10
@@ -49,16 +69,16 @@ while turns > 0:
     print('WELCOME TO BATTLESHIP')
     print_board(computer_board)
     row, column = get_ship_location()
-    if computer_board [row][column] == '-':
+    if computer_board[row][column] == '-':
         print('You already guessed that')
     elif computer_board[row][column] == 'X':
         print(' Congratulations, you have hit the battleship!')
         computer_board[row][column] = 'X'
         turns -= 1
-    else: 
+    else:
         print('Sorry you have missed')
         computer_board[row][column] = '-'
-        turn -= 1
+        turns -= 1
     if count_hits(computer_board) == 5:
         print('Congratulations, you have sunk all the battleships')
         break
